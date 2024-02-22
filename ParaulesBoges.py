23d0884ba8605dbf96055766649a6a59e473c9d7
cristian.taran.7e7 <cristian.taran.7e7@itb.cat>
2024-02-22 08:49:43 +0100

    bien

---
+++ ParaulesBoges.py
@@ -0,0 +1,8 @@
+"""
+Angela Plana, Victoria Ramos, Cristian Taran
+22/2/2024
+ASIXc M03 UF2 A1
+Descripción:Crear un programa  on es demani un nom d'usuari i una contrasenya i la quantitat  d’intents per fer login.
+Si l’usuari encerta la combinació de nom d’usuari i contrasenya, el programa li mostrarà el missatge “Benvingut al Sistema!”
+i una foto de la seva cara feta amb caràcters ASCII.
+"""
