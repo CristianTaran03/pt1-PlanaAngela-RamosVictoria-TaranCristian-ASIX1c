c2f8df524c4b36255b0e7c1696947d01bc169f8f
cristian.taran.7e7 <cristian.taran.7e7@itb.cat>
2024-02-22 12:15:40 +0100

    bien

--- ParaulesBoges.py
+++ ParaulesBoges.py
@@ -6,3 +6,20 @@ Descripción:Crear un programa  on es demani un nom d'usuari i una contrasenya i
 Si l’usuari encerta la combinació de nom d’usuari i contrasenya, el programa li mostrarà el missatge “Benvingut al Sistema!”
 i una foto de la seva cara feta amb caràcters ASCII.
 """
+import random
+
+lista = []
+def mezclar_letras(palabra):
+    for x in palabra:
+        primera_letra = x[0]
+        ultima_letra = x[-1]
+        letras_medio = list(x[1:-1])
+        random.shuffle(letras_medio)
+        palabra_mezclada = primera_letra + ''.join(letras_medio) + ultima_letra
+        lista.append(palabra_mezclada)
+    return lista
+palabra = input("Ingresa una palabra: ")
+palabra = palabra.split()
+print(palabra)
+lista = mezclar_letras(palabra)
+print(lista)
