0a81e476559a391b1bea296216f0008ae4abad51
cristian.taran.7e7 <cristian.taran.7e7@itb.cat>
2024-02-27 12:11:19 +0100

    bien

--- ParaulesBoges.py
+++ ParaulesBoges.py
@@ -2,24 +2,57 @@
 Angela Plana, Victoria Ramos, Cristian Taran
 22/2/2024
 ASIXc M03 UF2 A1
-Descripción:Crear un programa  on es demani un nom d'usuari i una contrasenya i la quantitat  d’intents per fer login.
-Si l’usuari encerta la combinació de nom d’usuari i contrasenya, el programa li mostrarà el missatge “Benvingut al Sistema!”
-i una foto de la seva cara feta amb caràcters ASCII.
+Descripción: Implementar en Python un programa que demani una frase per teclat i la retorni per pantalla amb les
+lletres de cada paraula de la frase desordenada, tal com diu l’estudi de la Universitat de Cambridge.
 """
 import random
+import string
 
-lista = []
-def mezclar_letras(palabra):
-    for x in palabra:
-        primera_letra = x[0]
-        ultima_letra = x[-1]
-        letras_medio = list(x[1:-1])
-        random.shuffle(letras_medio)
-        palabra_mezclada = primera_letra + ''.join(letras_medio) + ultima_letra
-        lista.append(palabra_mezclada)
-    return lista
-palabra = input("Ingresa una palabra: ")
-palabra = palabra.split()
-print(palabra)
-lista = mezclar_letras(palabra)
-print(lista)
+def crear_lista(text):
+    # Transformem l'imput en una llista, separant-lo per paraules.
+    llista_text = text.split()
+    return llista_text
+
+def mezcla_error(llista):
+    # Crear una nova llista on totes les paraules de la llista anterior estan canviades / Control d'errors.
+    resultat_llista = []
+    for x in llista:
+        primera_lletra = x[0]
+        ultima_lletra = x[-1]
+
+        if len(x) <= 2:
+            resultat_llista.append(x)
+        #si la palabra es un numero o un numero con decimales deja la palabra igual
+        elif x.isdigit():
+            resultat_llista.append(x)
+        #si la palabra es un numero con decimales deja la palabra igual
+        elif x.replace('.', '', 1).isdigit() or x.replace(',', '', 1).isdigit():
+            resultat_llista.append(x)
+        elif primera_lletra not in string.punctuation and ultima_lletra not in string.punctuation:
+            originals = list(x[1: -1])
+            random.shuffle(originals)
+            canviades = primera_lletra + ''.join(originals) + ultima_lletra
+            resultat_llista.append(canviades)
+        else:
+            segona_lletra = x[1]
+            penultima_lletra = x[-2]
+            originals = list(x[2: -2])
+            if len(x) > 5:
+                random.shuffle(originals)
+                canviades = primera_lletra + segona_lletra + ''.join(originals) + penultima_lletra + ultima_lletra
+                resultat_llista.append(canviades)
+            else:
+                resultat_llista.append(x)
+
+    return resultat_llista
+def resultat_string (resultat):
+    # Convertir la llista en una cadena nova com a resultat final.
+    resultat_final = " ".join(resultat)
+    print(resultat_final)
+    return resultat_final
+
+
+text = input()
+llista = crear_lista(text)
+resultat = mezcla_error(llista)
+frase_final = resultat_string(resultat)
