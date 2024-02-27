775b5940375acd6335be2dcf407f7b146485c483
Angela <angela.plana.7e7@itb.cat>
2024-02-27 20:32:17 +0100

    a

--- ParaulesBoges.py
+++ ParaulesBoges.py
@@ -2,9 +2,6 @@
 Angela Plana, Victoria Ramos, Cristian Taran
 27/02/2024
 ASIXc M03 UF2 A1
-Descripción:Crear un programa  on es demani un nom d'usuari i una contrasenya i la quantitat  d’intents per fer login.
-Si l’usuari encerta la combinació de nom d’usuari i contrasenya, el programa li mostrarà el missatge “Benvingut al Sistema!”
-i una foto de la seva cara feta amb caràcters ASCII.
 """
 import random
 import string
