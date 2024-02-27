2535cc975d3c70216d98db2c30f4c1a7b87740b0
Angela <angela.plana.7e7@itb.cat>
2024-02-27 19:00:40 +0100

    a

--- ParaulesBoges.py
+++ ParaulesBoges.py
@@ -9,7 +9,7 @@ i una foto de la seva cara feta amb caràcters ASCII.
 import random
 import string
 
-#region funcions
+#region Funcions
 def crear_lista(text):
     # Transformem l'input en una llista, separant-lo per paraules.
     llista_text = text.split()
