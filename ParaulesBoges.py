7d5127e9af8f9db90c768f666f2a1c392f4861ef
Angela <angela.plana.7e7@itb.cat>
2024-02-27 20:25:51 +0100

    a

--- ParaulesBoges.py
+++ ParaulesBoges.py
@@ -18,22 +18,24 @@ def crear_lista(text):
 def mezcla_error(llista):
     # Crear una nova llista on totes les paraules de la llista anterior estan canviades / Control d'errors.
     resultat_llista = []
+    signes = string.punctuation + '¡' + '¿'
+
     for paraula in llista:
         primera_lletra = paraula[0]
         ultima_lletra = paraula[-1]
 
-        if len(paraula) < 3:
+        if len(paraula) <= 3:
             resultat_llista.append(paraula)
 
         # Deixem els números tal com estan, ignorant els símbols. Així podem llegir preus, hores, fraccions, etc.
-        elif paraula.translate(str.maketrans("", "", string.punctuation)).isdigit():
+        elif paraula.translate(str.maketrans("", "", signes)).isdigit():
             resultat_llista.append(paraula)
 
         elif primera_lletra.isalpha() and ultima_lletra.isalpha():
             originals = list(paraula[1: -1])
 
             #Si qualsevol dels caràcters que hauríem de barrejar és un símbol, deixem la paraula tal com està. Així podem posar enllaços de webs, etc.
-            if any(char in string.punctuation for char in originals):
+            if any(char in signes for char in originals):
                 resultat_llista.append(paraula)
             else:
                 random.shuffle(originals)
@@ -41,26 +43,33 @@ def mezcla_error(llista):
                 resultat_llista.append(canviades)
 
         # La barreja en cas de que el primer i ultim caracter siguin signes de puntuació.
-        elif primera_lletra in string.punctuation and ultima_lletra in string.punctuation and len(paraula) >= 5:
+        elif primera_lletra in signes and ultima_lletra in signes and len(paraula) >= 5:
             segona_lletra = paraula[1]
             penultima_lletra = paraula[-2]
             originals = list(paraula[2: -2])
 
             # Tornem a deixar la paraula tal i com està si conté signes entre mig.
-            if any(char in string.punctuation for char in originals):
+            if any(char in signes for char in originals):
                 resultat_llista.append(paraula)
+
+            elif penultima_lletra in signes:
+                antep = paraula[-3]
+                originals = list(paraula[1: -3])
+                random.shuffle(originals)
+                canviades = primera_lletra + ''.join(originals) + antep + penultima_lletra + ultima_lletra
+                resultat_llista.append(canviades)
             else:
                 random.shuffle(originals)
                 canviades = primera_lletra + segona_lletra + ''.join(originals) + penultima_lletra + ultima_lletra
                 resultat_llista.append(canviades)
 
         # La barreja en cas de que el primer caracter sigui un signe de puntuació.
-        elif primera_lletra in string.punctuation and len(paraula) >= 4:
+        elif primera_lletra in signes and len(paraula) > 4:
             segona_lletra = paraula[1]
             originals = list(paraula[2: -1])
 
             # Tornem a deixar la paraula tal i com està si conté signes entre mig.
-            if any(char in string.punctuation for char in originals):
+            if any(char in signes for char in originals):
                 resultat_llista.append(paraula)
             else:
                 random.shuffle(originals)
@@ -68,13 +77,19 @@ def mezcla_error(llista):
                 resultat_llista.append(canviades)
 
         # La barreja en cas de que l'últim caracter sigui un signe de puntuació.
-        elif ultima_lletra in string.punctuation and len(paraula) >= 4:
+        elif ultima_lletra in signes and len(paraula) > 4:
             penultima_lletra = paraula[-2]
             originals = list(paraula[1: -2])
 
             # Tornem a deixar la paraula tal i com està si conté signes entre mig.
-            if any(char in string.punctuation for char in originals):
+            if any(char in signes for char in originals):
                 resultat_llista.append(paraula)
+            elif penultima_lletra in signes:
+                antep = paraula[-3]
+                originals = list(paraula[1: -3])
+                random.shuffle(originals)
+                canviades = primera_lletra + ''.join(originals) + antep + penultima_lletra + ultima_lletra
+                resultat_llista.append(canviades)
             else:
                 random.shuffle(originals)
                 canviades = primera_lletra + ''.join(originals) + penultima_lletra + ultima_lletra
