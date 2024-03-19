d9fe143132dbfd54950d279f2db45e061f7de079
angela-plana <angela.plana.7e7@itb.cat>
2024-03-19 12:32:02 +0100

    mierda

--- R1/ParaulesBoges.py
+++ R1/ParaulesBoges.py
@@ -16,7 +16,7 @@ def crear_lista(text):
 
 def mezcla_error(llista):
     # Crear una nova llista on totes les paraules de la llista anterior estan canviades / Control d'errors.
-    resultat_llista = []
+    resultat_final_llista = []
     signes = string.punctuation + '¡' + '¿'
 
     for paraula in llista:
@@ -24,22 +24,22 @@ def mezcla_error(llista):
         ultima_lletra = paraula[-1]
 
         if len(paraula) <= 3:
-            resultat_llista.append(paraula)
+            resultat_final_llista.append(paraula)
 
         # Deixem els números tal com estan, ignorant els símbols. Així podem llegir preus, hores, fraccions, etc.
         elif paraula.translate(str.maketrans("", "", signes)).isdigit():
-            resultat_llista.append(paraula)
+            resultat_final_llista.append(paraula)
 
         elif primera_lletra.isalpha() and ultima_lletra.isalpha():
             originals = list(paraula[1: -1])
 
             #Si qualsevol dels caràcters que hauríem de barrejar és un símbol, deixem la paraula tal com està. Així podem posar enllaços de webs, etc.
             if any(char in signes for char in originals):
-                resultat_llista.append(paraula)
+                resultat_final_llista.append(paraula)
             else:
                 random.shuffle(originals)
                 canviades = primera_lletra + ''.join(originals) + ultima_lletra
-                resultat_llista.append(canviades)
+                resultat_final_llista.append(canviades)
 
         # La barreja en cas de que el primer i ultim caracter siguin signes de puntuació.
         elif primera_lletra in signes and ultima_lletra in signes and len(paraula) >= 5:
@@ -49,18 +49,18 @@ def mezcla_error(llista):
 
             # Tornem a deixar la paraula tal i com està si conté signes entre mig.
             if any(char in signes for char in originals):
-                resultat_llista.append(paraula)
+                resultat_final_llista.append(paraula)
             # I si posem 2 signes al final? per exemple una paraula acaba amb ?,
             elif penultima_lletra in signes:
                 antep = paraula[-3]
                 originals = list(paraula[1: -3])
                 random.shuffle(originals)
                 canviades = primera_lletra + ''.join(originals) + antep + penultima_lletra + ultima_lletra
-                resultat_llista.append(canviades)
+                resultat_final_llista.append(canviades)
             else:
                 random.shuffle(originals)
                 canviades = primera_lletra + segona_lletra + ''.join(originals) + penultima_lletra + ultima_lletra
-                resultat_llista.append(canviades)
+                resultat_final_llista.append(canviades)
 
         # La barreja en cas de que el primer caracter sigui un signe de puntuació.
         elif primera_lletra in signes and len(paraula) > 4:
@@ -69,11 +69,11 @@ def mezcla_error(llista):
 
             # Tornem a deixar la paraula tal i com està si conté signes entre mig.
             if any(char in signes for char in originals):
-                resultat_llista.append(paraula)
+                resultat_final_llista.append(paraula)
             else:
                 random.shuffle(originals)
                 canviades = primera_lletra + segona_lletra + ''.join(originals) + ultima_lletra
-                resultat_llista.append(canviades)
+                resultat_final_llista.append(canviades)
 
         # La barreja en cas de que l'últim caracter sigui un signe de puntuació.
         elif ultima_lletra in signes and len(paraula) > 4:
@@ -82,7 +82,7 @@ def mezcla_error(llista):
 
             # Tornem a deixar la paraula tal i com està si conté signes entre mig.
             if any(char in signes for char in originals):
-                resultat_llista.append(paraula)
+                resultat_final_llista.append(paraula)
 
             # I si posem 2 signes al final? per exemple una paraula acaba amb ?,
             elif penultima_lletra in signes:
@@ -90,17 +90,17 @@ def mezcla_error(llista):
                 originals = list(paraula[1: -3])
                 random.shuffle(originals)
                 canviades = primera_lletra + ''.join(originals) + antep + penultima_lletra + ultima_lletra
-                resultat_llista.append(canviades)
+                resultat_final_llista.append(canviades)
             else:
                 random.shuffle(originals)
                 canviades = primera_lletra + ''.join(originals) + penultima_lletra + ultima_lletra
-                resultat_llista.append(canviades)
+                resultat_final_llista.append(canviades)
 
         # Que sempre retorni algo, encara que no canviï la paraula.
         else:
-            resultat_llista.append(paraula)
+            resultat_final_llista.append(paraula)
 
-    return resultat_llista
+    return resultat_final_llista
 def resultat_string (resultat):
     # Convertir la llista en una cadena nova com a resultat final.
     resultat_final = " ".join(resultat)
---
+++ R2.1/data_source.py
@@ -0,0 +1,8 @@
+
+def get_data__from_keyboard():
+    """ Aquesta funció recull les dades des del teclat
+    Retorna: una única cadena de caràcters"""
+    # Transformem l'input en una llista, separant-lo per paraules.
+    text = input()
+    return text
+
