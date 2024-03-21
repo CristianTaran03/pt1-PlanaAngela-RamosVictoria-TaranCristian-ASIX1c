7dee3dd137b33156724742fbc864f98223f50793
angela-plana <angela.plana.7e7@itb.cat>
2024-03-21 09:34:08 +0100

    mierda

--- R2.1/data_source.py
+++
@@ -1,8 +0,0 @@
-
-def get_data__from_keyboard():
-    """ Aquesta funció recull les dades des del teclat
-    Retorna: una única cadena de caràcters"""
-    # Transformem l'input en una llista, separant-lo per paraules.
-    text = input()
-    return text
-
--- R2.1/main.py
+++
@@ -1,6 +0,0 @@
-import crazy_words
-import data_source
-
-text = data_source.get_data__from_keyboard()
-crazy_words.principal(text)
-crazy_words.dividir_llista_paraules(text)
