8bb6709f3ebe48b10cccc55b6f9aabc88a3d65ad
cristian.taran.7e7 <cristian.taran.7e7@itb.cat>
2024-03-21 09:51:03 +0100

    bien

--- R2/main.py
+++ R2/main.py
@@ -7,8 +7,25 @@ La seva funcionalitat és obtenir les dades, processar-les per a obtenir les “
 """
 import crazy_words
 import data_source
+def demanar_opcio():
+    print("1. Introduir text per teclat")
+    print("2. Introduir URL")
+    print("3. Introduir text per ChatGPT")
+    opcio = input("Escull una opció: ")
+    if opcio == "1":
+        text = data_source.get_data__from_keyboard()
+        return text
+    elif opcio == "2":
+        text = data_source.get_data_from_server()
+        return text
+    elif opcio == "3":
+        text = data_source.get_data_from_chatGPT()
+        return text
+    else:
+        print("Opció incorrecta")
+        demanar_opcio()
 
-text = data_source.get_data_from_server()
+text = demanar_opcio()
 llista_texto = crazy_words.dividir_llista_paraules(text)
 resultat_final_llista, primera_lletra, ultima_lletra, signes = crazy_words.mix_words(llista_texto)
 crazy_words.resultat_string(resultat_final_llista)
