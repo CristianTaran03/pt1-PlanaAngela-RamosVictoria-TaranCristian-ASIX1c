78530e07765c07c8495f8961cae9e5bbc93dff28
cristian.taran.7e7 <cristian.taran.7e7@itb.cat>
2024-03-21 09:19:10 +0100

    bien

--- R2/data_source.py
+++ R2/data_source.py
@@ -1,29 +1,37 @@
 """
 Angela Plana, Victoria Ramos, Cristian Taran
 19/03/2024
-ASIXc M03 UF2 A1
+ASIXc M03 UF2 A2
 
 És l’encarregada d’obtenir les dades. En aquesta versió les demanarà per teclat. Tot i que és probable, que en posteriors versions, demani les dades per fitxer.
 Per tant, hauré d’implementar el mètode:
 """
 
-text = input()
-def get_data__from_keyboard(text):
+def get_data__from_keyboard():
     """ Aquesta funció recull les dades des del teclat
     Retorna: una única cadena de caràcters"""
-    # Transformem l'input en una llista, separant-lo per paraules.
-    llista_text = text.split()
-    return llista_text
+    text = input()
+    return text
 
-def get_data_from_server(URL):
-  """ Aquesta funció recull les dades des d’una API Rest a partir d’una URL
-URL example.txt   """
+def get_data_from_server():
+    """ Aquesta funció recull les dades des d’una API Rest a partir d’una URL
+    URL example.txt   """
+    import requests
+    import json
+    api_url = 'https://api.api-ninjas.com/v1/chucknorris'
+    response = requests.get(api_url, headers={'X-Api-Key': '4i+8EDg6uX7fc7Ws1YtQOA==yVzz3jXQOK7jlbpu'})
+    if response.status_code == requests.codes.ok:
+        data = json.loads(response.text)
+        text = data["joke"]
+        return text
+    else:
+        print("Error:", response.status_code, response.text)
 
-def get_data_from_chatGPT(questio):
- """ Aquesta funció recull la resposta a una pregunta """
+def get_data_from_chatGPT():
+    """ Aquesta funció recull la resposta a una pregunta """
 
 #TODO al 3r lliurament
-def get_data_from_file(file_name):
-   """ Aquesta funció recull les dades des d'un arxiu
- Entrada: Una cadena de caràcters amb el nom del fitxer origen
-       Retorna: una única cadena de caràcters"""
+def get_data_from_file():
+    """ Aquesta funció recull les dades des d'un arxiu
+    Entrada: Una cadena de caràcters amb el nom del fitxer origen
+    Retorna: una única cadena de caràcters"""
--- R2/main.py
+++ R2/main.py
@@ -1,14 +1,14 @@
 """
 Angela Plana, Victoria Ramos, Cristian Taran
 19/03/2024
-ASIXc M03 UF2 A1
+ASIXc M03 UF2 A2
 
-Per simplificar, i que tothom faci un disseny modular similar, és obligatori crear els arxius:
+La seva funcionalitat és obtenir les dades, processar-les per a obtenir les “paraules boges” i finalment mostrar-les per pantalla
 """
 import crazy_words
 import data_source
 
-def main():
-    print("Hello world")
-
-main()
+text = data_source.get_data_from_server()
+llista_texto = crazy_words.dividir_llista_paraules(text)
+resultat_final_llista, primera_lletra, ultima_lletra, signes = crazy_words.mix_words(llista_texto)
+crazy_words.resultat_string(resultat_final_llista)
